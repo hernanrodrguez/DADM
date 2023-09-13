dd6c003d639146b6b3e412be70fcbcab2d579fd5
Hernan Rodriguez <hrodriguez@kompusur.com.ar>
2023-09-13 00:34:28 -0300

    Agrego Floating Action Button en el dashboard

--- Application/app/src/main/java/com/example/application/fragments/TeamsDashboardFragment.kt
+++ Application/app/src/main/java/com/example/application/fragments/TeamsDashboardFragment.kt
@@ -14,6 +14,7 @@ import com.example.application.adapters.TeamAdapter
 import com.example.application.entities.Team
 import com.example.application.entities.TeamsRepository
 import com.example.application.entities.User
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 
 class TeamsDashboardFragment : Fragment() {
@@ -22,6 +23,7 @@ class TeamsDashboardFragment : Fragment() {
     private lateinit var recTeams : RecyclerView
     private lateinit var adapter : TeamAdapter
     private lateinit var snackbar : Snackbar
+    lateinit var fab: FloatingActionButton
 
     private var teamsRepository : TeamsRepository = TeamsRepository()
 
@@ -32,6 +34,11 @@ class TeamsDashboardFragment : Fragment() {
         v = inflater.inflate(R.layout.fragment_teams_dashboard, container, false)
 
         recTeams = v.findViewById(R.id.recTeams)
+        fab = v.findViewById(R.id.fab)
+
+        fab.setOnClickListener {
+            showSnackbar("FAB pressed!")
+        }
 
         return v
     }
