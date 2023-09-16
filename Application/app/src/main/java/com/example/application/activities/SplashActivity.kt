c5f6278633422e1b33c06216cc9abb23970f451a
Hernan Rodriguez <hrodriguez@kompusur.com.ar>
2023-09-16 19:26:22 -0300

    Agrego Splash Screen

---
+++ Application/app/src/main/java/com/example/application/activities/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.example.application.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.application.R
+
+class SplashActivity : AppCompatActivity() {
+
+    private val SPLASH_TIME_OUT:Long = 2000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed(
+
+            {
+                startActivity(Intent(this,MainActivity::class.java))
+                finish()
+            }
+            , SPLASH_TIME_OUT)
+    }
+}
